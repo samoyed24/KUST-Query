1c6a4950e9103a70885aa82222298de43a7bea9e
samoyed24 <142908795+samoyed24@users.noreply.github.com>
2023-11-25 23:14:49 +0800

    Add files via upload

---
+++ query/forms.py
@@ -0,0 +1,8 @@
+# from django import forms 
+
+# class QueryForm(forms.Form):
+#     username = forms.CharField(max_length=12)
+#     password = forms.CharField(max_length=200)
+#     output_mode = forms.IntegerField(max_value=2, required=True)
+
+
